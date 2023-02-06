172637c7cc3ee938b5fa1b43907b1f36641309f1
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-06 15:37:52 +0300

    game logic works

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+let playerSelection = prompt("Choose Rock, Paper or Scissors:").toLowerCase();
+let computerSelection = getComputerChoice();
+console.log(`comp choice is ${computerSelection}`);
+
 function getComputerChoice(){
     let rps = ["rock", "paper", "scissors"];
     let computerChoice = rps[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return "It's a TIE";
+    } else if (computerSelection === "rock" && playerSelection === "scissors"){
+        return "You Lose! Rock beats Scissors";
+    } else if (computerSelection === "paper" && playerSelection === "rock"){
+        return "You Lose! Paper beats Rock";
+    } else if (computerSelection === "scissors" && playerSelection === "paper"){
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "You Win! Scissors beats  Paper";
+    }
+}
+console.log(playRound(playerSelection, computerSelection));
